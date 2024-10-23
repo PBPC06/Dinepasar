13b26f993ffcb0d61901e8aea71a9480e9b6aab6
PBPC06 <pebepec06@gmail.com>
2024-10-23 11:18:25 +0700

    kelar login page

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from main.views import show_main, create_food_entry
+from main.views import show_main, create_food_entry, register, login_user, logout_user
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('create-food-entry', create_food_entry, name='create_food_entry')
+    path('create-food-entry', create_food_entry, name='create_food_entry'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,21 @@
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, redirect 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from main.forms import FoodEntryForm
 from main.models import FoodEntry
 
+@login_required(login_url='/login')
 def show_main(request):
     food_entries = FoodEntry.objects.all()
     context = {
         'test': 'test',
-        'food_entries': food_entries
+        'food_entries': food_entries,
+        'last_login': request.COOKIES['last_login'],
     }
     return render(request, "main.html", context)
 
@@ -21,3 +28,37 @@ def create_food_entry(request):
 
     context = {'form': form}
     return render(request, "create_food_entry.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
